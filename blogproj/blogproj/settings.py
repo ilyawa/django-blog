c6684fbb56d174864b14559270eb4d2d9434dc53
ilyawa <ilyawa.work@yahoo.com>
2024-02-07 22:24:20 +0300

    updated reqs

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ['https://django-blog-svdv.onrender.com', 'localhost']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+#ALLOWED_HOSTS = ['https://django-blog-svdv.onrender.com', 'localhost']
 
 # Application definition
 
